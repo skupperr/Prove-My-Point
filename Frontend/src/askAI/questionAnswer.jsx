079f8e3118b61c0a09599df47509fe8eea9615c9
skupperr <mylifeasasif@gmail.com>
2025-07-14 00:35:50 +0600

    responsive UI

--- Frontend/src/askAI/questionAnswer.jsx
+++ Frontend/src/askAI/questionAnswer.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useLocation } from "react-router-dom";
 import { useApi } from "../utils/api";
 import { useAuth } from "@clerk/clerk-react";
@@ -12,6 +12,7 @@ export default function QuestionAnswer() {
 
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
+    const textareaRef = useRef(null); // 👈 Add textarea ref
 
     useEffect(() => {
         const fetchEntry = async () => {
@@ -30,8 +31,17 @@ export default function QuestionAnswer() {
             }
         };
 
+
+
         fetchEntry();
     }, [userId, docId]);
+
+    useEffect(() => {
+        if (textareaRef.current) {
+            textareaRef.current.style.height = "auto";
+            textareaRef.current.style.height = `${textareaRef.current.scrollHeight}px`;
+        }
+    }, [data]);
 
     if (loading)
         return (
@@ -62,7 +72,18 @@ export default function QuestionAnswer() {
                                     <path d="M229.66,218.34l-50.07-50.06a88.11,88.11,0,1,0-11.31,11.31l50.06,50.07a8,8,0,0,0,11.32-11.32ZM40,112a72,72,0,1,1,72,72A72.08,72.08,0,0,1,40,112Z" />
                                 </svg>
                             </div>
-                            <input className="search-input" type="text" defaultValue={question} disabled={true} />
+                            <textarea
+                                className="search-input"
+                                name="text"
+                                wrap="soft"
+                                defaultValue={question}
+                                disabled={true}
+                                style={{ overflow: 'hidden', resize: 'none' }}
+                                ref={textareaRef}
+                            />
+
+
+                            {/* <input className="search-input" type="text" defaultValue={question} disabled={true} /> */}
                             <div className="search-clear">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 256 256">
                                     <path d="M165.66,101.66,139.31,128l26.35,26.34a8,8,0,0,1-11.32,11.32L128,139.31l-26.34,26.35a8,8,0,0,1-11.32-11.32L116.69,128,90.34,101.66a8,8,0,0,1,11.32-11.32L128,116.69l26.34-26.35a8,8,0,0,1,11.32,11.32ZM232,128A104,104,0,1,1,128,24,104.11,104.11,0,0,1,232,128Zm-16,0a88,88,0,1,0-88,88A88.1,88.1,0,0,0,216,128Z" />
