0fa0ad30c01af32e1308333146bd738efff43323
skupperr <mylifeasasif@gmail.com>
2025-07-14 01:02:33 +0600

    responsive UI

--- Frontend/src/askAI/askAI.jsx
+++ Frontend/src/askAI/askAI.jsx
@@ -47,57 +47,62 @@ export default function AskAI({ inputText, setInputText, onSubmit }) {
                     </div>
                     <p>Powered by AI, answers are based on published research papers.</p>
 
-                    <div className='sample-search'>
-                        <div
-                            className="sample-search-1"
-                            onClick={() => {
-                                const question = "Can solar flares disrupt GPS?";
-                                setInputText(question);
-                                onSubmit(question);
-                            }}
-                        >
-                            <p>Can solar flares disrupt GPS?</p>
-                            <i className="fa-solid fa-magnifying-glass" style={{ color: '#8f9194', marginRight: '1rem', marginTop: '0.4rem' }}></i>
+                    <div className='parent-sample-search'>
+                        <div className='sample-search'>
+                            <div
+                                className="sample-search-1"
+                                onClick={() => {
+                                    const question = "Can solar flares disrupt GPS?";
+                                    setInputText(question);
+                                    onSubmit(question);
+                                }}
+                            >
+                                <p>Can solar flares disrupt GPS?</p>
+                                <i className="fa-solid fa-magnifying-glass" style={{ color: '#8f9194', marginRight: '1rem', marginTop: '0.4rem' }}></i>
+                            </div>
+
+                            <div
+                                className="sample-search-1"
+                                onClick={() => {
+                                    const question = "What is the impact of AI on education?";
+                                    setInputText(question);
+                                    onSubmit(question);
+                                }}
+                            >
+                                <p>What is the impact of AI on education?</p>
+                                <i className="fa-solid fa-magnifying-glass" style={{ color: '#8f9194', marginRight: '1rem', marginTop: '0.4rem' }}></i>
+                            </div>
+
                         </div>
 
-                        <div
-                            className="sample-search-1"
-                            onClick={() => {
-                                const question = "What is the impact of AI on education?";
-                                setInputText(question);
-                                onSubmit(question);
-                            }}
-                        >
-                            <p>What is the impact of AI on education?</p>
-                            <i className="fa-solid fa-magnifying-glass" style={{ color: '#8f9194', marginRight: '1rem', marginTop: '0.4rem' }}></i>
-                        </div>
 
-                    </div>
-                    <div className='sample-search'>
-                        <div
-                            className="sample-search-1"
-                            onClick={() => {
-                                const question = "Can local birds recognize human faces?";
-                                setInputText(question);
-                                onSubmit(question);
-                            }}
-                        >
-                            <p>Can local birds recognize human faces?</p>
-                            <i className="fa-solid fa-magnifying-glass" style={{ color: '#8f9194', marginRight: '1rem', marginTop: '0.4rem' }}></i>
-                        </div>
 
-                        <div
-                            className="sample-search-1"
-                            onClick={() => {
-                                const question = "Why do phone screens affect our sleep?";
-                                setInputText(question);
-                                onSubmit(question);
-                            }}
-                        >
-                            <p>Why do phone screens affect our sleep?</p>
-                            <i className="fa-solid fa-magnifying-glass" style={{ color: '#8f9194', marginRight: '1rem', marginTop: '0.4rem' }}></i>
-                        </div>
+                        <div className='sample-search'>
+                            <div
+                                className="sample-search-1"
+                                onClick={() => {
+                                    const question = "Can local birds recognize human faces?";
+                                    setInputText(question);
+                                    onSubmit(question);
+                                }}
+                            >
+                                <p>Can local birds recognize human faces?</p>
+                                <i className="fa-solid fa-magnifying-glass" style={{ color: '#8f9194', marginRight: '1rem', marginTop: '0.4rem' }}></i>
+                            </div>
 
+                            <div
+                                className="sample-search-1"
+                                onClick={() => {
+                                    const question = "Why do phone screens affect our sleep?";
+                                    setInputText(question);
+                                    onSubmit(question);
+                                }}
+                            >
+                                <p>Why do phone screens affect our sleep?</p>
+                                <i className="fa-solid fa-magnifying-glass" style={{ color: '#8f9194', marginRight: '1rem', marginTop: '0.4rem' }}></i>
+                            </div>
+
+                        </div>
                     </div>
                 </div>
             </div>
